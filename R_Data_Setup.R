2b41855ef012c19c3b6b689b0367089d22a94545
bmahardja <brianmahardja@gmail.com>
2020-12-22 13:17:09 -0800

    Rename folder 12/22/20

---
+++ R_Data_Setup.R
@@ -0,0 +1,20 @@
+
+#Install some github packages to pull Sam Bashevkin's integrated dataset
+
+#library(devtools)
+#devtools::install_github("sbashevkin/spacetools")
+#install.packages("remotes")
+#remotes::install_github("sbashevkin/deltareportr")
+
+
+library(tidyverse)
+library(deltareportr)
+library(lubridate)
+library(hms)
+library(rgdal)
+library(sf)
+library(broom)
+library(mgcv)
+library(units)
+
+######
