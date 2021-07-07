6565aa02719133da0d1206f350b2c22345e0aae5
bmahardja <brianmahardja@gmail.com>
2021-07-07 13:43:40 -0700

    Spatiotemporal variogram code updated 7/7/21

--- R_Spatiotemporal_Variogram.R
+++ R_Spatiotemporal_Variogram.R
@@ -13,6 +13,7 @@ library(ggpubr)
 library(gtable)
 library(grid)
 library(gridExtra)
+library(plotly)
 
 require(spacetime)
 require(sp)
@@ -29,9 +30,11 @@ results_root<-file.path("results")
 
 #To get UTM data
 Delta.aut <- readOGR(file.path(data_root,"Bay_Delta_Poly_Outline3_UTM10", "Bay_Delta_Poly_Outline_NoSSC_UTM10.shp"))
+#EPSG 4269
 
 #Dataset
 temp_dataset <- readRDS("temperature_dataset.Rds")
+temp_dataset$Date<-as.Date(temp_dataset$Date)
 
 #Read saved model results
 model_05_soapfilm_xy_jd_ta<-readRDS(file.path(results_root,"Model_05_soapfilm.Rds"))
@@ -40,24 +43,105 @@ model_05_soapfilm_xy_jd_ta<-readRDS(file.path(results_root,"Model_05_soapfilm.Rd
 #Function to create variogram from Sam
 ######################################################################################################
 
+Data_vario<-temp_dataset%>%
+  mutate(Resid=resid_gam(model_05_soapfilm_xy_jd_ta))
+
+Data_coords<-Data_vario%>%
+  st_as_sf(coords=c("x", "y"), crs=4269)%>%
+  st_coordinates()%>%
+  as_tibble()#%>%
+  #mutate(across(c(X,Y), ~(.x-mean(.x))/1000)) #From Sam. I believe this was to convert to kilometers
+
+#Create data frame for the variogram analysis
+Data_vario<-bind_cols(Data_vario%>%
+                        select(Date, Resid), Data_coords)
+
+sp<-SpatialPoints(coords=data.frame(X=Data_vario$X, Y=Data_vario$Y))
+
+sp2<-STIDF(sp, time=Data_vario$Date, 
+           data=data.frame(Residuals=Data_vario$Resid))
+
+#Calculate gamma
+vario_01<-variogramST(Residuals~1, data=sp2, tunit="days", cores=2, tlags=0:14)
+vario_02<-variogramST(Residuals~1, data=sp2, tunit="weeks", cores=2, tlags=1:8)
+
+#Adjust the 0.5
+vario_plot_01<-vario_01%>%
+  mutate(spacelag_km=spacelag/1000,timelag_day=timelag-0.5)
+
+vario_plot_02<-vario_02%>%
+  mutate(spacelag_km=spacelag/1000,timelag_week=timelag-0.5)
+
+p<-plot_ly(data=vario_plot_01,x=vario_plot_01$spacelag_km,y=vario_plot_01$timelag_day,z=vario_plot_01$gamma,type="mesh3d",
+           intensity=vario_plot_01$gamma, colorscale="Viridis")
+p<-p %>% layout(
+  scene = list(
+    xaxis = list(title = "Distance (km)"),
+    yaxis = list(title = "Time difference (days)"),
+    zaxis = list(title = "Gamma")
+  ))
+p
+
+
+p2<-plot_ly(data=vario_plot_02,x=vario_plot_02$spacelag_km,y=vario_plot_02$timelag_week,z=vario_plot_02$gamma,type="mesh3d",
+           intensity=vario_plot_02$gamma, colorscale="Viridis")
+p2<-p2 %>% layout(
+  scene = list(
+    xaxis = list(title = "Distance (km)"),
+    yaxis = list(title = "Time difference (weeks)"),
+    zaxis = list(title = "Gamma")
+  ))
+p2
+
+
+
+
+
+
+###############Don't use
+######################################################################################################
+
+#If necessary to do what Sam did
+p_time<-ggplot(vario_plot, aes(x=timelag, y=gamma, color=spacelag, group=spacelag))+
+  geom_line()+
+  geom_point()+
+  scale_color_viridis_c(name="Distance (km)")+
+  #scale_x_continuous(breaks=c(2,4,6,8,10))+
+  xlab("Time difference (months)")+
+  theme_bw()+
+  theme(legend.justification = "left")
+
+p_time
+
+p_space<-ggplot(vario_plot, aes(x=spacelag, y=gamma, color=timelag, group=timelag))+
+  geom_line()+
+  geom_point()+
+  scale_color_viridis_c(breaks=c(2,4,6,8,10), name="Time difference\n(months)")+
+  xlab("Distance (km)")+
+  theme_bw()+
+  theme(legend.justification = "left")
+
+p_space
+
+#####
 ST_variogram<-function(model, data, cores){
-  norm_resids<-resid_gam(model, incl_na=TRUE)
+  norm_resids<-resid_gam(model)
   
   Data_vario<-data%>%
     mutate(Resid=norm_resids)
   
   Data_coords<-Data_vario%>%
-    st_as_sf(coords=c("x", "y"), crs=26910)%>%
+    st_as_sf(coords=c("x", "y"), crs=4269)%>%
     st_coordinates()%>%
-    as_tibble()%>%
-    mutate(across(c(X,Y), ~(.x-mean(.x))/1000))
+    as_tibble()#%>%
+    #mutate(across(c(X,Y), ~(.x-mean(.x))/1000))
   
   Data_vario<-bind_cols(Data_vario%>%
                           select(Date, Resid), Data_coords)
   sp<-SpatialPoints(coords=data.frame(X=Data_vario$X, Y=Data_vario$Y))
   sp2<-STIDF(sp, time=Data_vario$Date, 
              data=data.frame(Residuals=Data_vario$Resid))
-  vario<-variogramST(Residuals~1, data=sp2, tunit="weeks", cores=cores, tlags=(30/7)*0:10)
+  vario<-variogramST(Residuals~1, data=sp2, tunit="weeks", cores=cores, tlags=1:8)
   
   vario_plot<-vario%>%
     mutate(monthlag=as.integer(as.factor(timelag))-0.5)
